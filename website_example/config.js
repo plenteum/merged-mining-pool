0734171dc0389d321e659163b45b7bfcad996f25
Dave <5268997+davehlong@users.noreply.github.com>
2019-09-09 08:54:34 +0200

    Initial Commit

---
+++ website_example/config.js
@@ -0,0 +1,17 @@
+var apiMerged = "https://arq.pool.plenteum.com";
+var api = "https://ple.pool.plenteum.com";
+
+var email = "support@plenteum.com";
+var telegram = "https://t.me/plenteum";
+var discord = "http://chat.plenteum.com";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var blockchainExplorerMerged = "https://blockexplorer.arqma.com/block/{id}";
+var transactionExplorerMerged = "https://blockexplorer.arqma.com/tx/{id}";
+
+var blockchainExplorer = "http://block-explorer.plenteum.com/?hash={id}#blockchain_block";
+var transactionExplorer = "http://block-explorer.plenteum.com/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
